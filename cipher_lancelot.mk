20cac007fff3432bcaf5d3b3d73f9ac4f6e8124f
kitw4y <91876095+kitw4y@users.noreply.github.com>
2023-03-15 03:14:34 +0700

    Update and rename lineage_lancelot.mk to cipher_lancelot.mk

--- cipher_lancelot.mk
+++ cipher_lancelot.mk
@@ -11,10 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from Cipher Common
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lancelot
+# CipherOS specific flags
+# Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+# Faceunlock Support
+TARGET_FACE_UNLOCK_SUPPORTED := true
+# Maintainer
+CIPHER_MAINTAINER := kitw4y
+# GMS
+CIPHER_GAPPS := true
+# Enable Blurs
+CIPHER_BLUR := true
+
+PRODUCT_NAME := cipher_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
